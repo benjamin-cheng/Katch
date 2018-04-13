a146dfb33326e5c87701e8a3221fd22b07e1fe02
Benjamin Cheng <chengwc@gmail.com>
2018-04-13 14:24:50 +0800

    Add empty AddDownloadTaskActivity

---
+++ app/src/main/java/io/katch/add/AddDownloadTaskActivity.kt
@@ -0,0 +1,13 @@
+package io.katch.add
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.katch.R
+
+class AddDownloadTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_downlaod_task)
+    }
+}
